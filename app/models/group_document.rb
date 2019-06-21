ee58ec8d44d4b7fe35a852fdb21ee7f8345239b6
Farz Bhullar <farz@codegaragetech.com>
2019-06-21 15:49:37 +0530

    missing file

---
+++ app/models/group_document.rb
@@ -0,0 +1,27 @@
+class GroupDocument
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  include DocumentUploader::Attachment.new(:document)
+
+  embedded_in :group
+  
+  belongs_to :upload_document
+
+  field :document_name,           type: String, default: ""
+  field :document_data,   		  type: String, default: ""
+  field :status, 				  type: String, default: "pending"
+  field :total_pages, type: Integer,default: 0
+  field :processed_pages, type: Integer,default: 0
+
+  validates :status, inclusion: { in: ['ready_for_payment', 'ready_for_print','processinging','failed', 'completed'] }
+
+  def is_document_added_to_group?
+  	flag = false
+  	if user.group.present? and user.group.document_ids.include?(self.id.to_s)
+  		flag = true
+  	end
+  	return flag
+  end
+  
+end
