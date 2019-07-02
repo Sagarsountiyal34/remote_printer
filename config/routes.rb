7b7dd39981466f84dc19672dba2b5155639192e6
Farz Bhullar <farz@codegaragetech.com>
2019-07-02 13:10:51 +0530

    update group status api and bug fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'group/view'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 	root to: 'groups#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
