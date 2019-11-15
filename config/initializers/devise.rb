48ac4e2cf78a630cd0efb31268b043e0f42ec2d4
Farz Bhullar <farz@codegaragetech.com>
2019-11-15 14:56:35 +0530

    UploadDocument Page design changes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,9 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'ashish@codegaragetech.com'
+  config.mailer_sender = "\"AP Computer\" noreply@Apcomprinter.com"
+
+  # config.mailer_sender = "\"noreply@Apcomprinter.com\""
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
